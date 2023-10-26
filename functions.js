8c282f8bd93ca1e185df58d083469cc6a6ae1add
ColinTreanor <130803296+ColinTreanor@users.noreply.github.com>
2023-10-26 09:56:34 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -4,9 +4,9 @@ function validate() {
 
 
     if (pass1.length < 8) {
-        alert("The length of the first password is less than 8 letters!")
+        alert("The length of the first password is less than 8 letters")
     } else if (pass2.length < 8) {
-        alert("The length of the second password is less than 8 letters!")
+        alert("The length of the second password is less than 8 letters")
     } else if (pass1 != pass2) {
         alert("Two passwords don't match!")
     } else {
